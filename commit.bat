115501a5ba8f80daf26fb62b576dcf94adc5a269
Mabenan <doener123@googlemail.com>
2020-06-02 15:02:26 +0200

    added icons

--- commit.bat
+++ commit.bat
@@ -1,11 +1,11 @@
 git add .
-git commit -m "%1"
+git commit -m %1
 git push
 cd reps
 for /d %%d in (*.*) do (
     cd "%%d"
     git add .
-    git commit -m "%1"
+    git commit -m %1
     git push
     cd ..
 )
